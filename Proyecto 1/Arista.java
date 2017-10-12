dd5f0b5881e21ed74da4bbcdc7e5c141e4b1beaa
David Segura <dsegura95@hotmail.com>
2017-10-12 12:26:18 -0400

    Add files via upload

---
+++ Proyecto 1/Arista.java
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+
+public class Arista extends Lado
+{
+  private Vertice u;
+  private Vertice v;
+  
+  public Arista(String id, double peso, Vertice u, Vertice v) {
+  }
+
+  public Vertice getExtremo1() {
+  }
+
+  public Vertice getExtremo2() {
+  }
+
+  public String toString() {
+  }
+}
---
+++ Proyecto 1/Lado.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+public abstract class Lado
+{
+  private String id;
+  private double peso;
+
+  public Lado(String id, double peso) {
+  }
+
+  public String getId() {
+  }
+
+  public double getPeso() {
+  }
+
+  public abstract String toString();
+
+}
